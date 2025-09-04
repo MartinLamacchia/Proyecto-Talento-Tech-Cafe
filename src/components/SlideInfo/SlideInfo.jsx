ed29a674f819d9f4592bcc5f8e221261eca2c436
MartinLamacchia <martinjiro22@gmail.com>
2025-09-04 14:47:39 -0300

    Haciendo el Slider del proyecto

--- src/components/SlideInfo/SlideInfo.jsx
+++ src/components/SlideInfo/SlideInfo.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import styles from './SlideInfo.module.css'
+import React, { useEffect, useState } from "react";
+import styles from "./SlideInfo.module.css";
+
+let paragraphSlider = [
+  {
+    first: "Consigue un 20% de descuento en compras superiores a $50.000",
+    second: "un 15% superiores a $30.000 o 10% superiores a $20.000",
+  },
+  {
+    first: "Envios 24/48hs",
+    second: "Clientes VIP, tus envios en 24/48hs. Unite y obtene mas info.",
+  },
+  {
+    first: "Devoluciones",
+    second:
+      "Clientes VIP, tiene 30 dias para hacer la devolucion. Unite y obtene mas info.",
+  },
+  {
+    first: "Promociones",
+    second:
+      "Clientes VIP, hasta 50% de descuento en productos seleccionados. Unite y obtene mas info.",
+  },
+];
 
 const SlideInfo = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % paragraphSlider.length);
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className={styles.container}>
-        <p className={styles.firstParagraph}>Consigue un 20% de descuento en compras superiores a $50.000</p>
-        <p className={styles.secondParagraph}>un 15% superiores a $30.000 o 10% superiores a $20.000</p>
+      <p className={styles.firstParagraph}>{paragraphSlider[index].first}</p>
+      <p className={styles.secondParagraph}>{paragraphSlider[index].second}</p>
     </div>
-  )
-}
+  );
+};
 
-export default SlideInfo
+export default SlideInfo;
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,23 +3,23 @@ import styles from './Home.module.css'
 import Nav from "../../components/Nav/Nav";
 import SlideInfo from "../../components/SlideInfo/SlideInfo";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
-import CategoryProducts from "../../components/CategoryProducts/CategoryProducts";
-import Hero from "../../components/Hero/Hero";
-import Footer from "../../components/Footer/Footer";
+import CategoryProducts from '../../components/CategoryProducts/CategoryProducts'
+import Hero from '../../components/Hero/Hero'
+import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
   return (
     <>
       <Nav />
       <SlideInfo />
-      <div className={styles.containerFeaturedProducts}>
+      <div className={styles.container}>
         <FeaturedProducts />
       </div>
-      <div className={styles.containerFeaturedProducts} >
+      <div className={styles.container}>
         <CategoryProducts />
       </div>
       <Hero />
-      <Footer />
+      <Footer/>
     </>
   );
 };
